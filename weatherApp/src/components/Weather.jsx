f6ec2a743d33083578fb95dac38fc9af9feb6b89
Manu <manu1316a@gmail.com>
2025-05-20 18:27:54 +0530

    added search bar and serach icons and imported images

--- weatherApp/src/components/Weather.jsx
+++ weatherApp/src/components/Weather.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './Weather.css'
+import React from "react";
+import "./Weather.css";
+import searchIcon from "../assets/search.png";
+import clearIcon from '../assets/clear.png'
+import cloudIcon from '../assets/cloud.png'
+import drizzleIcon from '../assets/drizzle.png'
+import rainIcon from '../assets/rain.png'
+import snowIcon from '../assets/snow.png'
+import windIcon from '../assets/wind.png'
+import humidityIcon from '../assets/humidity.png'
 
 const Weather = () => {
   return (
     <div>
-      <h1>Weather</h1>
-    </div>
-  )
-}
+      <div className="weather">
+        <div className="search-bar">
+          <input type="text" placeholder="search" />
+          <img src={searchIcon} alt="" />
+        </div>
 
-export default Weather
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
